152507afc5c9d01a3a1ad2ec85955a1b5aa931cc
yura <uragaiduk@gmail.com>
2019-07-24 23:02:51 +0300

    WOrkingn on set handler

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import webapp2
+from google.appengine.ext import ndb
+
+
+class NameValuePair(ndb.Model):
+    """A main model for representing an individual Guestbook entry."""
+    name = ndb.StringProperty(indexed=False)
+    value = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
 
 class GetHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World! ' + self.request.get("name"))
+        name_to_search = self.request.get("name")
+        name_value_query = NameValuePair.query(NameValuePair.name=name_to_search)
+        name_value_pair = name_value_query.fetch(1)
+        self.response.write(name_value_pair)
+
+
+class SetHandler(webapp2.RequestHandler):
+    def get(self):
+        name = self.request.get("name")
+        value = self.request.get("value")
+        name_value_pair = NameValuePair()
+        name_value_pair.name = name
+        name_value_pair.value = value
+        name_value_pair.put()
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(name + '=' + value)
 
 
 app = webapp2.WSGIApplication([
     ('/get', GetHandler),
+    ('/set', SetHandler)
 ], debug=True)
