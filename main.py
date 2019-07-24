d64c9900c71be0f3707c737814fe692bfcbcec0b
yura <uragaiduk@gmail.com>
2019-07-25 02:02:39 +0300

    Added undo

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import webapp2
 from google.appengine.ext import ndb
 
 LAST = 'last'
-
-
-def get_by_name(name_to_search):
-    name_value_query = NameValuePair.query(ndb.AND(NameValuePair.name == name_to_search, NameValuePair.status == LAST))
-    name_value_pair_query_result = name_value_query.fetch(1)
-    return name_value_pair_query_result
+PREV = 'prev'
 
 
 class NameValuePair(ndb.Model):
@@ -15,6 +10,37 @@ class NameValuePair(ndb.Model):
     value = ndb.StringProperty(indexed=True)
     status = ndb.StringProperty(indexed=True)
     date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+def get_by_name(name_to_search, status_to_search):
+    name_value_query = NameValuePair.query(
+        ndb.AND(NameValuePair.name == name_to_search, NameValuePair.status == status_to_search)).order(
+        -NameValuePair.date)
+    name_value_pair_query_result = name_value_query.fetch(1)
+    return name_value_pair_query_result
+
+
+class UndoHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        name_value_query = NameValuePair.query(NameValuePair.status == LAST).order(-NameValuePair.date)
+        name_value_pair_query_result = name_value_query.fetch(1)
+        if len(name_value_pair_query_result):
+
+            current_value = name_value_pair_query_result[0]
+            prev_value_result = NameValuePair.query(NameValuePair.date < current_value.date).order(
+                -NameValuePair.date).fetch(1)
+            if len(prev_value_result):
+                prev_value = prev_value_result[0]
+                current_value.status = PREV
+                current_value.put()
+                prev_value.status = LAST
+                prev_value.put()
+                self.response.write(prev_value.name + "=" + (prev_value.value or 'None'))
+            else:
+                self.response.write("NO COMMANDS")
+        else:
+            self.response.write("NO COMMANDS")
 
 
 class EndHandler(webapp2.RequestHandler):
@@ -42,13 +68,19 @@ class NumEqualToHandler(webapp2.RequestHandler):
 
 class UnsetHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        name_to_search = self.request.get("name")
-        name_value_query = NameValuePair.query(NameValuePair.name == name_to_search).order(-NameValuePair.date)
-        name_value_pair_query_result = name_value_query.fetch(1)
-        name_value_pair = name_value_pair_query_result[0]
+        name = self.request.get("name")
+        value = self.request.get("value")
+        pair_by_name = get_by_name(name, LAST)
+        if len(pair_by_name):
+            # Updating last entry
+            pair_by_name[0].status = PREV
+            pair_by_name[0].put()
+        name_value_pair = NameValuePair()
+        name_value_pair.name = name
         name_value_pair.value = None
+        name_value_pair.status = LAST
         name_value_pair.put()
+        self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(name_value_pair.name + "=None")
 
 
@@ -56,7 +88,7 @@ class GetHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         name_to_search = self.request.get("name")
-        name_value_pair_query_result = get_by_name(name_to_search)
+        name_value_pair_query_result = get_by_name(name_to_search, LAST)
         if len(name_value_pair_query_result):
             name_value_pair = name_value_pair_query_result[0]
             self.response.write(name_value_pair.name + "=" + (name_value_pair.value or 'None'))
@@ -68,10 +100,10 @@ class SetHandler(webapp2.RequestHandler):
     def get(self):
         name = self.request.get("name")
         value = self.request.get("value")
-        pair_by_name = get_by_name(name)
+        pair_by_name = get_by_name(name, LAST)
         if len(pair_by_name):
-            # Updating last entery
-            pair_by_name[0].status = ''
+            # Updating last entry
+            pair_by_name[0].status = PREV
             pair_by_name[0].put()
         name_value_pair = NameValuePair()
         name_value_pair.name = name
@@ -87,6 +119,7 @@ app = webapp2.WSGIApplication([
     ('/set', SetHandler),
     ('/unset', UnsetHandler),
     ('/numequalto', NumEqualToHandler),
+    ('/undo', UndoHandler),
     ('/showall', ShowAllHandler),
     ('/end', EndHandler)
 ], debug=True)
