b39edb6b2278bd9c9e651509c032e0c5e4aded6d
yura <uragaiduk@gmail.com>
2019-07-25 01:03:46 +0300

    Added more handlers

--- main.py
+++ main.py
@@ -1,31 +1,82 @@
 import webapp2
 from google.appengine.ext import ndb
 
+LAST = 'last'
+
+
+def get_by_name(name_to_search):
+    name_value_query = NameValuePair.query(ndb.AND(NameValuePair.name == name_to_search, NameValuePair.status == LAST))
+    name_value_pair_query_result = name_value_query.fetch(1)
+    return name_value_pair_query_result
+
 
 class NameValuePair(ndb.Model):
-    """A main model for representing an individual Guestbook entry."""
     name = ndb.StringProperty(indexed=True)
     value = ndb.StringProperty(indexed=True)
+    status = ndb.StringProperty(indexed=True)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 
-class GetHandler(webapp2.RequestHandler):
+class EndHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        ndb.delete_multi(NameValuePair.query().fetch(keys_only=True))
+        self.response.write('CLEANED')
+
+
+class ShowAllHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        all_rows = NameValuePair.query().fetch()
+        self.response.write(all_rows)
+
+
+class NumEqualToHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        value_to_search = self.request.get("value")
+        amount = NameValuePair.query(
+            ndb.AND(NameValuePair.value == value_to_search, NameValuePair.status == LAST)).count()
+        self.response.write(amount)
+
+
+class UnsetHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         name_to_search = self.request.get("name")
         name_value_query = NameValuePair.query(NameValuePair.name == name_to_search).order(-NameValuePair.date)
         name_value_pair_query_result = name_value_query.fetch(1)
         name_value_pair = name_value_pair_query_result[0]
-        self.response.write(name_value_pair.name + "=" + name_value_pair.value)
+        name_value_pair.value = None
+        name_value_pair.put()
+        self.response.write(name_value_pair.name + "=None")
+
+
+class GetHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        name_to_search = self.request.get("name")
+        name_value_pair_query_result = get_by_name(name_to_search)
+        if len(name_value_pair_query_result):
+            name_value_pair = name_value_pair_query_result[0]
+            self.response.write(name_value_pair.name + "=" + (name_value_pair.value or 'None'))
+        else:
+            self.response.write("Not found")
 
 
 class SetHandler(webapp2.RequestHandler):
     def get(self):
         name = self.request.get("name")
         value = self.request.get("value")
+        pair_by_name = get_by_name(name)
+        if len(pair_by_name):
+            # Updating last entery
+            pair_by_name[0].status = ''
+            pair_by_name[0].put()
         name_value_pair = NameValuePair()
         name_value_pair.name = name
         name_value_pair.value = value
+        name_value_pair.status = LAST
         name_value_pair.put()
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(name + '=' + value)
@@ -33,5 +84,9 @@ class SetHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/get', GetHandler),
-    ('/set', SetHandler)
+    ('/set', SetHandler),
+    ('/unset', UnsetHandler),
+    ('/numequalto', NumEqualToHandler),
+    ('/showall', ShowAllHandler),
+    ('/end', EndHandler)
 ], debug=True)
